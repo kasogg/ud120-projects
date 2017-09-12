61c0bf979aa5652c1fcc55e55d978c28f116dacd
KasoGG <kasogg@gmail.com>
2017-09-12 23:56:35 +0800

    提交代码

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 """ 
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
 
@@ -9,25 +8,26 @@
     Sara has label 0
     Chris has label 1
 """
-    
-import sys
-from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
 
+import sys
+sys.path.append("../tools/")
+from time import time
+from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
 
+gnb = GaussianNB()
+gnb.fit(features_train, labels_train)
+y_pred = gnb.predict(features_test)
+
+print gnb.score(features_test, labels_test)
+print y_pred
 
 #########################################################
-
-
